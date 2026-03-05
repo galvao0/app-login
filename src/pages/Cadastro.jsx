78fa0db48c6336cda77be1f3f4024df02dc8e9b1
Pedro Nascimento Galvão <cin0127306@aluno.firjansenaisesi.com.br>
2026-03-05 15:55:27 -0300

    insert

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
 export function Cadastro () {
+
+    const [login, setLogin] = useState('')
+    const [senha, setSenha] = useState('')
+    const navigate = useNavigate()
+
+    async function cadastrar (e) 
+    {
+        e.preventDefault()
+
+        await fetch('http://localhost:3020/usuario', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                login,
+                senha
+            })
+        })
+        alert('usuario cadstrado')
+        navigate('/')
+    }
+
     return (
-        <>
-            <h1>Cadastro</h1>
-        </>
+        <div>
+            <h2>Novo Cadastro</h2>
+
+            <form onSubmit={cadastrar}>
+                <input 
+                    placeholder="login"
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                /> <br /> <br />
+                <input 
+                    type="password"
+                    placeholder="senha"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                /> <br /> <br />
+                <button type="submit">Cadastra</button>
+            </form> <br />
+            <Link to={'/'}>Login</Link>
+        </div>
     )
 }
